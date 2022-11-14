0d8f5658398334c6102d98948628a54d1e6a2162
AdrianBottini <adrianboh@gmail.com>
2022-11-14 18:23:38 +0100

    nuovo rifatto

--- script/script.js
+++ script/script.js
@@ -1,65 +1,64 @@
-new Vue ({
-	el: ".container-blur",
-	data: {
-		arrImages: [
-			{
-				image: '01.webp',
-				title: "Marvel's Spiderman Miles Morale",
-				text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-			},
-			{
-				image: '02.webp',
-				title: 'Ratchet & Clank: Rift Apart',
-				text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-			},
-			{
-				image: '03.webp',
-				title: 'Fortnite',
-				text: 'Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.',
-			},
-			{
-				image: '04.webp',
-				title: 'Stray',
-				text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-			},
-			{
-				image: '05.webp',
-				title: "Marvel's Avengers",
-				text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
-			},
-		],
-
-		timeSlider: 1.5 * 1000,
-		direction: 1,
-		activeIndex: 0,
-		idInterval: 0,
-		isAutoplayActive: true,
-	}
-	methods : {
-		changeSlide: moveRight() {
-			listEleImg[activeIndex].classList.remove('active');
-			listThumbs[activeIndex].classList.remove('active');
-		
-			activeIndex++;
-			
-			if (activeIndex === listEleImg.length) {
-				activeIndex = 0;
-			}
-		
-			listEleImg[activeIndex].classList.add('active');
-			listThumbs[activeIndex].classList.add('active');
-		}
-		changeSlide: moveLeft() {
-			listEleImg[activeIndex].classList.remove('active');
-			listThumbs[activeIndex].classList.remove('active');
-		
-			if (activeIndex === 0) {
-				activeIndex = listEleImg.length;
-			}
-			activeIndex--;
-		
-			listEleImg[activeIndex].classList.add('active');
-			listThumbs[activeIndex].classList.add('active');
-		}
-	}
-});
+var app = new Vue(
+    {
+        el: "#app",
+        data:   {
+            actualElement: 0,
+            sliderInterval: false,
+            slides: [
+                {
+                    image: 'img/01.webp',
+                    title: "Marvel's Spiderman Miles Morale",
+                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                },
+                {
+                    image: 'img/02.webp',
+                    title: 'Ratchet & Clank: Rift Apart',
+                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                },
+                {
+                    image: 'img/03.webp',
+                    title: 'Fortnite',
+                    text: 'Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.',
+                },
+                {
+                    image: 'img/04.webp',
+                    title: 'Stray',
+                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                },
+                {
+                    image: 'img/05.webp',
+                    title: "Marvel's Avengers",
+                    text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+                }
+            ],
+        },
+        methods: {
+            nextElement(){
+                if(this.actualElement === 4){
+                    this.actualElement = 0;
+                } else{
+                    this.actualElement += 1;
+                }
+            },
+            previousElement(){
+                if(this.actualElement === 0){
+                    this.actualElement = 4;
+                } else{
+                    this.actualElement -= 1;
+                }
+            },
+            setActualElement(index){
+                this.actualElement = index;
+            },
+            carouselStop(){
+                clearInterval(sliderInterval);
+            },
+            carouselStart(){
+                sliderInterval = setInterval(this.nextElement, 5000);
+            }
+        },
+        mounted() {
+            sliderInterval = setInterval(this.nextElement, 5000);
+        }
+    }
+)
