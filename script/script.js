3042e6fefbf88a0813e9a7b1815acb3472f95e03
AdrianBottini <adrianboh@gmail.com>
2022-11-14 17:15:39 +0100

    img

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ new Vue ({
 	data: {
 		arrImages: [
 			{
-				image: 'img/01.webp',
+				image: '01.webp',
 				title: "Marvel's Spiderman Miles Morale",
 				text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
 			},
